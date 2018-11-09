83315a6630318d1f746f062476dd601e23932489
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-11-09 01:26:39 -0600

    configuring project"

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatInputModule
+} from "@angular/material";
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatButtonModule, MatIconModule, MatCardModule, MatInputModule]
+})
+export class MaterialModule {}
